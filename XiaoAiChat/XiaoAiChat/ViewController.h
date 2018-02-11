b5728d1234924aff57891c88f68a6adce0bc3055
wangyaoguo <wangyaoguo@wangyaoguodeMac-mini.local>
2018-02-11 17:23:00 +0800

    no message

---
+++ XiaoAiChat/XiaoAiChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiaoAiChat
+//
+//  Created by wangyaoguo on 2018/2/11.
+//  Copyright © 2018年 lianluo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
